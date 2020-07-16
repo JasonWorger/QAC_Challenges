cde42261d78f5e7e43b784c8b6a306f26aa425c1
Jason Worger <jasonworger@gmail.com>
2020-07-16 18:08:22 +0100

    Challenge 1

---
+++ julydevops.py
@@ -0,0 +1,29 @@
+# Making a List
+julyDevops = ["Christopher", "John", "Jason", "Arsalan", "Domenico", "Joshua", "Ryan", "Bradley", "Javas", "Sithembiso",
+              "Jacob", "Clifford", "Wasim", "Mohamed", "Tobias", "Christopher", "Samuel", "Edmund", "Amanda", "Steven",
+              "Jack"];
+
+# Adding Luke to the list
+julyDevops.append("Luke")
+print(julyDevops)
+
+# Printing the 5th name in the list
+print(julyDevops[4])
+
+## Count method 1
+
+# Using the count function for the name Chris in the list
+julyDevops.count("Christopher")
+
+print(julyDevops.count("Christopher"))
+
+## Count method 2 *Better Method*
+
+#Creating the count variable to print the number of Chris
+count = julyDevops.count("Christopher")
+
+# Printing the number of Chris
+print("The number of Christopher in cohort is:", count)
+
+
+
