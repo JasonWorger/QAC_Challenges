d6d9a5d74e05c094de217a4646c3c4e609ebd7f4
Jason Worger <jasonworger@gmail.com>
2020-07-28 08:39:01 +0100

    Seven Not Five Challenge and Test

--- SevenNotFive.py
+++ SevenNotFive.py
@@ -2,7 +2,7 @@ def sevennotfive(start,end):
     numbersbyseven= []
     for x in range(start, end):
         if (x%7==0) and (x%5!=0):
-            numbersbyseven.append(x)
-    return numbersbyseven
+            numbersbyseven.append(str(x))
+    return ','.join(numbersbyseven)
 
-print(sevennotfive(2000,3200))
+print(sevennotfive(2000,3201))
